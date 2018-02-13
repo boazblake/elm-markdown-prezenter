c20d646069bfdbd128e53c3a1949fe37e88bf795
bblake <bblake@goempyrean.com>
2018-02-12 18:28:48 -0600

    updated semantics prez

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ var server = jsonServer.create()
 // Set Default middlewares ( logger, static, cors and no-cache)
 server.use(jsonServer.defaults())
 
-var router = jsonServer.router('./dbs/semantics.json')
+var router = jsonServer.router('./dbs/semantics/semantics.json')
 server.use(router)
 
 console.log('listening on 4000')
