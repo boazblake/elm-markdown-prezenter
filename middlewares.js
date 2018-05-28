982f8747e3cf29d94b3367dc6b78f35699b0dade
Boaz Blake <bblake@goempyrean.com>
2018-05-28 07:21:12 -0500

    updating dbs

---
+++ middlewares.js
@@ -0,0 +1,26 @@
+let db = require('./dbs/OO/oo/db.json');
+
+let counter = 0;
+const id = () => counter + 1;
+
+module.exports = (req, res, next) => {
+	console.log('slides', db);
+	console.log('id', id);
+	console.log(req.body);
+
+	switch (req.method) {
+		case 'POST':
+			db.slides[id()] = req.body;
+			console.log('POSTING SLIDES>git stta>>>>>>>\n', db);
+			next();
+			break;
+		case 'GET':
+			console.log('FETCHING SLIDES>>>>\n', db);
+			next();
+			break;
+		default:
+			console.log('error', req.method);
+			next();
+			break;
+	}
+};
